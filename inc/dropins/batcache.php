f540ac3054775bc7d5419f2ddd48387ef700d694
Taylor Lovett <tlovett88@gmail.com>
2016-05-24 00:26:01 -0400

    Add cache exemption functionality to batcache

--- inc/dropins/batcache.php
+++ inc/dropins/batcache.php
@@ -378,6 +378,46 @@ if ( empty( $batcache ) && ! empty($GLOBALS['sc_config'] ) && isset( $GLOBALS['s
 	);
 }
 
+// Deal with optional cache exceptions
+if ( ! empty( $GLOBALS['sc_config']['cache_exception_urls'] ) ) {
+	$exceptions = preg_split( '#(\n|\r)#', $GLOBALS['sc_config']['cache_exception_urls'] );
+
+	foreach ( $exceptions as $exception ) {
+		if ( preg_match( '#^[\s]*$#', $exception ) ) {
+			continue;
+		}
+
+		$exception = trim( $exception );
+
+		if ( preg_match( '#^https?://#', $exception ) ) {
+
+			$exception = rtrim( $exception, '/' );
+			$url = rtrim( 'http' . ( isset( $_SERVER['HTTPS'] ) ? 's' : '' ) . '://' . "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}", '/' );
+
+			if ( strtolower( $exception ) === strtolower( $url ) ) {
+				// Exception match!
+				return;
+			}
+
+		} elseif ( preg_match( '#^/#', $exception ) ) {
+			$path = $_SERVER['REQUEST_URI'];
+
+			if ( '/' !== $path ) {
+				$path = rtrim( $path, '/' );
+			}
+
+			if ( '/' !== $exception ) {
+				$exception = rtrim( $exception, '/' );
+			}
+
+			if ( strtolower( $exception ) === strtolower( $path ) ) {
+				// Exception match!
+				return;
+			}
+		}
+	}
+}
+
 $batcache = new batcache( $batcache );
 
 if ( ! defined( 'WP_CONTENT_DIR' ) ) {
