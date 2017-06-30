bb2ed0f9f3cc2752ac470c8427504801e22b590d
Benoît Chantre <mail@benoitchantre.com>
2017-07-01 01:11:46 +0200

    Set interval for hours in filter_cron_schedules()

--- inc/class-sc-cron.php
+++ inc/class-sc-cron.php
@@ -36,7 +36,9 @@ class SC_Cron {
 
 			$interval = $config['page_cache_length'] * MINUTE_IN_SECONDS;
 
-			if ( ! empty( $config['page_cache_length'] ) && 'days' === $config['page_cache_length'] ) {
+			if ( ! empty( $config['page_cache_length'] ) && 'hours' === $config['page_cache_length'] ) {
+				$interval = $config['page_cache_length'] * HOUR_IN_SECONDS;
+			} elseif ( ! empty( $config['page_cache_length'] ) && 'days' === $config['page_cache_length'] ) {
 				$interval = $config['page_cache_length'] * DAY_IN_SECONDS;
 			} elseif ( ! empty( $config['page_cache_length'] ) && 'weeks' === $config['page_cache_length'] ) {
 				$interval = $config['page_cache_length'] * WEEK_IN_SECONDS;
